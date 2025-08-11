945d6b468c924e0f62b99c21a0f0c11917d515c0
atmajo chowdhury <atmajoc@gmail.com>
2025-08-11 23:37:58 +0530

    feat: add updateProfile controller and integrate it into userRouter

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,12 +1,14 @@
 import { deleteUser } from "@/controllers/user/deleteUser";
 import { getUsers } from "@/controllers/user/getUsers";
 import { profile } from "@/controllers/user/profile/profile";
+import { updateProfile } from "@/controllers/user/profile/updateProfile";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.get("/profile", profile);
+router.patch("/profile", updateProfile);
 router.delete("/:id", deleteUser);
 
 export { router as userRouter };
