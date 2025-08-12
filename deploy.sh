0243ca4b568146306e105838fa2b6914c1c68188
atmajo chowdhury <atmajoc@gmail.com>
2025-08-12 22:15:35 +0530

    feat: refactor deployment process by moving Docker commands to deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+cd times-real-estate
+
+echo 'Stopping Docker containers...'
+docker compose down --remove-orphans
+
+echo 'Cleaning up Docker images...'
+docker image prune -f
+            
+echo 'Pulling latest changes...'
+git pull origin dev
+
+echo 'Building and starting containers...'
+docker compose up --build -d
+            
+echo 'Deployment complete. Running containers:'
+docker ps
